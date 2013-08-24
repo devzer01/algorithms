923b6a5668ec2f306c88381af6a8e23e9d39117b
Nayana <nayana@corp-gems.com>
2013-08-24 11:50:33 +0700

    commiting

--- Algorithms/src/com/gems/algorithms/BinarySearch.java
+++ Algorithms/src/com/gems/algorithms/BinarySearch.java
@@ -25,17 +25,32 @@ public class BinarySearch {
 		
 		return -1;
 	}
+	
+	
+	
+	public static int findSqrt(int key)
+	{
+		
+		int l = 0;
+		int u = 65536;
+
+		while (l+1 <= u) {
+			
+			int mid = l + (u - l) / 2;
+			int midSqr = mid * mid;
+			
+			if (key == midSqr) return mid;
+			
+			if (midSqr > key) {
+				u = mid;
+			} else {
+				l = mid;
+			}
+		}
+		return -1;
+	}
 		
 	public static void main(String[] args) {		
-		int[] array = new int[10];
-		Random rand = new Random();
-		for (int i = 0; i < array.length; i++)
-		    array[i] = rand.nextInt(100) + 1;		
-		Arrays.sort(array);
-		
-		int index = BinarySearch.find(array, array[7]);
-		
-		System.out.println("Return of the index is " + index + "\n");
-		
+		System.out.println("Sqrt of " + (49 * 49) + " -- " + BinarySearch.findSqrt(49 * 49));		
 	}
 }
